868f74bb34ad0ee914b83e3a79c3b78d634d21ca
DESKTOP-UE1AEIF\marce <gicu.buruiana@gmail.com>
2019-08-10 19:22:10 -0400

    no message

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(helmet.contentSecurityPolicy({
     directives: {
-    defaultSrc: ["'self', 'http:', 'https:'"],
+    defaultSrc: ["'self', 'http://*', 'https://*'"],
       styleSrc: ["'self'"],
           connectSrc: [
         "'self'",
