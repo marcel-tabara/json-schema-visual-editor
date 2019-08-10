330adb072af8d91c154312c19c18755c40951179
DESKTOP-UE1AEIF\marce <gicu.buruiana@gmail.com>
2019-08-10 17:14:53 -0400

    no message

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(helmet.contentSecurityPolicy({
     ],
     }
 }));
+app.use(favicon(__dirname + '/docs/favicon.ico'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
